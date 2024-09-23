f3eef401b96b278528c2bd6ecf23ccdaf866d7e4
Eduardo Lima <dudulj15@gmail.com>
2024-09-23 00:45:42 -0300

    Add BarLineChartDemo and BasicBarChartDemo components to display bar and line charts

---
+++ ui/src/components/Examples/Charts/BarLineChartDemo.tsx
@@ -0,0 +1,30 @@
+import { Bar, CartesianGrid, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
+import { chartConfig, chartData } from "../DemoChart";
+
+export const BarLineChartDemo = () => {
+    return (
+        <div style={{ width: "100%", height: 300 }}>
+            {/* Assegura que o ResponsiveContainer tenha altura */}
+            <ResponsiveContainer>
+                <ComposedChart data={chartData}>
+                    <CartesianGrid />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar
+                        dataKey="desktop"
+                        fill={chartConfig.desktop.color}
+                        name="Desktop"
+                    />
+                    <Line
+                        type="monotone"
+                        dataKey="mobile"
+                        stroke={chartConfig.mobile.color}
+                        name="Mobile"
+                    />
+                </ComposedChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
