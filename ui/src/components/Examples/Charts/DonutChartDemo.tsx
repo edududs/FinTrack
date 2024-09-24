649b8eaf21e5a2036d4c5f72430f142bd7ba5976
Eduardo Lima <dudulj15@gmail.com>
2024-09-23 21:35:12 -0300

    Refactor DonutChartDemo to adjust the height of the chart

--- ui/src/components/Examples/Charts/DonutChartDemo.tsx
+++ ui/src/components/Examples/Charts/DonutChartDemo.tsx
@@ -25,7 +25,7 @@ const DonutChartDemo: React.FC = () => {
   };
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="100%" height={250}>
       <PieChart>
         <Pie
           data={dataPieChart}
