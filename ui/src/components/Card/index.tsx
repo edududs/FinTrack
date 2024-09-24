08bd8f758db8bfdc1eb0caea3202c42123b5d318
Eduardo Lima <dudulj15@gmail.com>
2024-09-23 21:25:40 -0300

    Add Card component to display a styled card with header, footer, title, description, and content

---
+++ ui/src/components/Card/index.tsx
@@ -0,0 +1,11 @@
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./CardBase";
+
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
