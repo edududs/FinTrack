c7543b41015136550e54ca52d53a7724f520cdc1
Eduardo Lima <dudulj15@gmail.com>
2024-09-23 21:38:54 -0300

    Refactor DemoComponents to update component imports and modify text content

--- ui/src/pages/DemoComponents/DemoComponents.tsx
+++ ui/src/pages/DemoComponents/DemoComponents.tsx
@@ -11,14 +11,27 @@ import DemoCalendar from "src/components/Examples/DemoCalendar";
 import { DialogDemo } from "src/components/Examples/DemoDialog";
 import { DialogCloseButton } from "src/components/Examples/DemoDialogShare";
 import DialogMenuDemo from "src/components/Examples/DemoDialogMenu";
-import DemoChart from "src/components/Examples/DemoChart";
+import DemoCharts from "src/components/Examples/DemoCharts";
+import { Component as TooltipDemo } from "src/components/Chart/Tooltip/TooltipDemo";
+import { Label } from "src/components/Label";
+import CardDemo from "src/components/Examples/DemoCard";
+import { ComboboxForm } from "src/components/ComboBox/ComboBoxForm";
+import { AspectRatioDefault } from "src/components/AspectRatio/AspectRatioDemo";
+import { AvatarImage, Avatar, AvatarFallback } from "src/components/Avatar";
+import DemoCarousels from "src/components/Examples/DemoCarousels";
 
 const DemoComponents: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center max-w-[50%] gap-4 ">
-      <h1 className="text-5xl text-center">Olá,</h1>
-      <p className="text-center text-xl">
-        seja bem vindo ao meu ambiente de teste de componentes
+      <div>
+        <Avatar>
+          <AvatarImage src="https://cdn-icons-png.flaticon.com/512/5024/5024509.png" />
+          <AvatarFallback>JS</AvatarFallback>
+        </Avatar>
+      </div>
+      <h1 className="text-5xl text-center font-bold mb-4">Hello,</h1>
+      <p className="text-center text-xl mb-6">
+        Welcome to my component testing environment
       </p>
       <Separator />
       <Accordion triggerText="Toaster">
@@ -29,7 +42,7 @@ const DemoComponents: React.FC = () => {
         <DropDownMenuDemo />
       </Accordion>
 
-      <Accordion triggerText="Modais">
+      <Accordion triggerText="Modals">
         <div className="flex flex-wrap gap-3">
           <DemoConfirmationModal />
           <DemoModal />
@@ -47,12 +60,34 @@ const DemoComponents: React.FC = () => {
         <ProfileForm />
       </Accordion>
 
-      <Accordion triggerText="Calendário">
+      <Accordion triggerText="Calendar">
         <DemoCalendar />
       </Accordion>
 
-      <Accordion triggerText="Gráfico">
-        <DemoChart />
+      <Accordion triggerText="Graphs">
+        <DemoCharts />
+        <Separator className="mt-7 mb-7" />
+        <Label className="text-xl justify-center flex mb-3">
+          Exemplos de Tooltip
+        </Label>
+        <TooltipDemo />
+      </Accordion>
+
+      <Accordion triggerText="Card">
+        <CardDemo />
+      </Accordion>
+
+      <Accordion triggerText="ComboBox Form">
+        <ComboboxForm />
+      </Accordion>
+
+      <Accordion triggerText="Aspect Ratio">
+        <Label>With Aspect Ratio</Label>
+        <AspectRatioDefault />
+      </Accordion>
+
+      <Accordion triggerText="Carousels">
+        <DemoCarousels />
       </Accordion>
     </div>
   );
