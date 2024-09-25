c10109dc48e891d016bad69b8b4657825ded3b7b
Eduardo <natos.10@hotmail.com>
2024-09-25 08:48:14 -0300

    Add Examples component and its sub-components

---
+++ ui/src/components/Examples/index.tsx
@@ -0,0 +1,76 @@
+import { DemoFormSwitches } from "./DemoFormSwitches";
+import ButtonDemoToast from "./DemoToast";
+import { DemoModal } from "./Modals/DemoModals";
+import DemoConfirmationModal from "./Modals/DemoConfirmationModal";
+import DropDownMenuDemo from "src/components/core/DropdownMenus/DropDownMenuExample";
+import DemoCalendar from "./DemoCalendar";
+import { DialogDemo } from "./DemoDialog";
+import { DialogCloseButton } from "./DemoDialogShare";
+import DialogMenuDemo from "./DemoDialogMenu";
+import DemoCharts from "./DemoCharts";
+import { Component as TooltipDemo } from "src/components/core/Chart/Tooltip/TooltipDemo";
+import CardDemo from "./DemoCard";
+import DemoCarousels from "./DemoCarousels";
+import { ComboboxForm } from "../core/ComboBox/ComboBoxForm";
+import { SingleControllerAccordion } from "../core/Accordion/AccordionDefault";
+import { HoverCardDemo } from "./DemoHoverCard";
+import { TabsDemo } from "./DemoTabs";
+import { TableDemo } from "./Table/TableDemo";
+import { ProfileForm } from "../core/Forms/FormDemo";
+import { AspectRatioDefault } from "../core/AspectRatio/AspectRatioDemo";
+
+import {
+  Accordion,
+  AdaptiveTooltip,
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Label,
+  Separator,
+} from "src/components/core";
+
+export {
+  Accordion,
+  AdaptiveTooltip,
+  AspectRatioDefault,
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  ButtonDemoToast,
+  CardDemo,
+  ComboboxForm,
+  DemoCalendar,
+  DemoCarousels,
+  DemoCharts,
+  DemoConfirmationModal,
+  DemoFormSwitches,
+  DemoModal,
+  Dialog,
+  DialogCloseButton,
+  DialogContent,
+  DialogDemo,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogMenuDemo,
+  DialogTitle,
+  DialogTrigger,
+  DropDownMenuDemo,
+  HoverCardDemo,
+  Label,
+  ProfileForm,
+  Separator,
+  SingleControllerAccordion,
+  TableDemo,
+  TabsDemo,
+  TooltipDemo,
+};
